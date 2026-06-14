74fb3b5eab2bf5ea9627a220d6fc119ae10ad2be
Kshitij Panday <kshitijpanday06@gmail.com>
2026-06-14 11:53:21 +0530

    Time: 1 ms (73.32%) | Memory: 42.7 MB (83.03%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,15 +1,15 @@
 class Solution {
     public String interpret(String command) {
         StringBuilder ans = new StringBuilder();
-        for(int i = 0; i<command.length();i++){
-            if(command.charAt(i) == 'G'){
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
                 ans.append('G');
-            }
-            if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+            } else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
                 ans.append('o');
-            }
-            if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                i++;
+            } else {
                 ans.append("al");
+                i += 3;
             }
         }
         return ans.toString();

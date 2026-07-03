29c1486043aa57df786366474619fb808cffd7ba
Kshitij Panday <kshitijpanday06@gmail.com>
2026-07-03 16:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (83.49%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int matrixScore(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 0) {
+                for (int j = 0; j < n; j++) {
+                    grid[i][j] ^= 1;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            int zeros = 0;
+            int ones = 0;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 0) {
+                    zeros++;
+                } else
+                    ones++;
+            }
+            if (zeros > ones) {
+                for (int i = 0; i < m; i++) {
+                    grid[i][j] ^= 1;
+                }
+            }
+        }
+        int sum = 0;
+        int pow = 1;
+        for (int j = n - 1; j >= 0; j--) {
+            int ones = 0;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 1) {
+                    ones++;
+                }
+            }
+            sum += pow * ones;
+                pow *= 2;
+        }
+        return sum;
+    }
+}

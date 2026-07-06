2e24bcd0503f02e3aa5c198f0ac21cbe711b540b
Kshitij Panday <kshitijpanday06@gmail.com>
2026-07-06 22:18:02 +0530

    Time: 1 ms (97.66%) | Memory: 42.7 MB (22.97%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        long ans = 0;
+       long start = 1;
+       long end = x;
+
+       while ( start <= end){
+        long mid = start + (end - start)/2;
+
+        if(mid*mid == x){
+            return (int) mid;
+        }
+        else if(mid*mid > x){
+            
+            end = mid - 1;
+
+        }
+        else if (mid*mid <x){
+            ans = mid;
+            start = mid + 1;
+        }
+        
+       } 
+       return (int)ans;
+    }
+}
